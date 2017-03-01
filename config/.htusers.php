c64f2f75f9d8297f36d7d77198c9480fe891133c
wangxiaomo <wxm4ever@gmail.com>
2017-03-01 22:17:50 +0800

    init eXtplorer

---
+++ config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$lP1RD/C9YESVK85Ep7XQducmrg7vMif2Qr0Tfp5vUyl4/dujrbcXy','/data/env/www','http://dev.minmore.com','1','','7',1),
+); 
+?>
